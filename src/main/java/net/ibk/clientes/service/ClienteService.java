1edf15d53e1ecad8f2171afc9880254a755358a4
numa <numatorres@gmail.com>
2022-07-13 09:43:23 -0500

    cambios adicionales

--- src/main/java/net/ibk/clientes/service/ClienteService.java
+++ src/main/java/net/ibk/clientes/service/ClienteService.java
@@ -4,8 +4,9 @@ import net.ibk.clientes.model.Cliente;
 import net.ibk.clientes.model.ClienteDTO;
 import net.ibk.clientes.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,21 +28,26 @@ public class ClienteService {
     public void updateCliente(String codigoUnico, Cliente clienteN) {
         if (verificarCliente(clienteN)) {
             Optional<Cliente> cliente = clienteRepository.findClientesByCodigoUnico(codigoUnico);
-            cliente.get().setIdTipoDocumento(clienteN.getIdTipoDocumento());
-            cliente.get().setNumeroDocumento(clienteN.getNumeroDocumento());
-            cliente.get().setNombres(clienteN.getNombres());
-            cliente.get().setApellidos(clienteN.getApellidos());
-            clienteRepository.save(cliente.get());
+            if (cliente.isPresent()) {
+                cliente.get().setIdTipoDocumento(clienteN.getIdTipoDocumento());
+                cliente.get().setNumeroDocumento(clienteN.getNumeroDocumento());
+                cliente.get().setNombres(clienteN.getNombres());
+                cliente.get().setApellidos(clienteN.getApellidos());
+                clienteRepository.save(cliente.get());
+            }
+            else {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            }
         } else {
             throw new IllegalStateException("Error: Datos de Cliente Incompletos");
         }
     }
 
     private boolean verificarCliente(Cliente cliente) {
-        return (cliente.getApellidos().length() == 0 ||
-                cliente.getNombres().length() == 0 ||
-                cliente.getNumeroDocumento().length() == 0 ||
-                cliente.getIdTipoDocumento() < 1 ||
-                cliente.getIdTipoDocumento() > 3) ? false : true;
+        return cliente.getApellidos().length() != 0 &&
+                cliente.getNombres().length() != 0 &&
+                cliente.getNumeroDocumento().length() != 0 &&
+                cliente.getIdTipoDocumento() >= 1 &&
+                cliente.getIdTipoDocumento() <= 3;
     }
 }
